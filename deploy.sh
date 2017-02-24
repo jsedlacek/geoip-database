80559342e896064e733ac66817b7b27f57697c27
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2017-02-25 00:24:43 +0100

    remove rubish

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@
 git config --global user.email "sedlacek.jakub@gmail.com"
 git config --global user.name "Travis CI"
 git remote add upstream https://$GH_TOKEN@github.com/jsedlacek/geoip-database
-https://github.com/jsedlacek/geoip-database.git
 npm version patch
 git push upstream head
