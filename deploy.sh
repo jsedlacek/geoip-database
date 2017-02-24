8408cf1681f6dfe183f3e99cde29e98f5e4811d2
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2017-02-25 00:29:29 +0100

    just version using build number

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-git config --global user.email "sedlacek.jakub@gmail.com"
-git config --global user.name "Travis CI"
-git remote add upstream https://$GH_TOKEN@github.com/jsedlacek/geoip-database
-npm version patch
-git push upstream head
+npm version 0.2.$TRAVIS_BUILD_NUMBER
