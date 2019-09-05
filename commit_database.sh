082303ff92e233c15e74c89d2985ab9aab7e5366
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2019-09-05 20:49:34 +0200

    Update commit_database.sh

--- commit_database.sh
+++ commit_database.sh
@@ -1,13 +1,14 @@
 git config --global user.email "sedlacek.jakub@gmail.com"
 git config --global user.name "CI"
 
+header=$(echo -n "ad-m:${GITHUB_TOKEN}" | base64)
 
 if [[ `git status --porcelain` ]]; then
   git add -A
   git commit -m 'update database files'
   npm version patch
-  git push origin HEAD:master
-  git push --tag
+  git -c http.extraheader="AUTHORIZATION: basic $header" push origin HEAD:master
+  git -c http.extraheader="AUTHORIZATION: basic $header" push --tag
   npm publish
 else
   echo 'no changes'
