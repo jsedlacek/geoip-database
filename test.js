82b619612b37eeb9350ec213bf977b4cc6474c73
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2019-09-05 10:54:35 +0200

    fix test with new ip

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const maxmind = require('maxmind');
 
 test('city', t => {
   const cityLookup = maxmind.openSync(database.city);
-  const {city, country, location} = cityLookup.get('89.22.72.9');
+  const { city, country, location } = cityLookup.get('83.208.93.244');
   t.equal(city.names.en, 'Prague');
   t.equal(country.names.en, 'Czechia');
   t.equal(location.time_zone, 'Europe/Prague');
@@ -13,7 +13,7 @@ test('city', t => {
 
 test('country', t => {
   const countryLookup = maxmind.openSync(database.country);
-  const {country} = countryLookup.get('83.208.93.244');
+  const { country } = countryLookup.get('83.208.93.244');
   t.equal(country.names.en, 'Czechia');
   t.end();
 });
